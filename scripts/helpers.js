bd9e5f58ba3a65699783ddd2ce3d030303c75437
zhi-hu.wang <zhi-hu.wang@atos.net>
2018-09-11 01:55:36 +0800

    add baidu and growingio

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -113,7 +113,7 @@ hexo.extend.helper.register('url_for_lang', function(path) {
 });
 
 hexo.extend.helper.register('raw_link', function(path) {
-	return 'https://github.com/hexojs/site/edit/master/source/' + path;
+	return 'https://github.com/aimacity/aimacity/edit/master/source/' + path;
 });
 
 hexo.extend.helper.register('page_anchor', function(str) {
